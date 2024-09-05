2478a3f4772ab2ecf22354a35c2766523f947d5f
thepowerKey888 <thepower674@gmail.com>
2024-09-05 11:17:19 -0500

    Initial commit: Added hello.txt, test.txt, and logs.log

---
+++ main.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5};
+		System.out.println("The average is: " +
+		average(numbers));
+	}
+// Non-working average function (needs a fix)
+	public static double average(int[] nums) {
+		
+		double cur_total = 0;
+		for(int i = 0; i < nums.length; i++){
+			
+			cur_total = cur_total + nums[i];
+		}
+		
+		double avg = cur_total/nums.length;
+		return avg;
+	}
+}
